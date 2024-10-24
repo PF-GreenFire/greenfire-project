c1357be6b09107d067c03ce2c4d23cdf42252078
1sol2 <lhs1119r@gmail.com>
2024-10-24 23:06:45 +0900

    set: 백엔드 기본 환경설정

---
+++ src/main/java/sisosolsol/greenfire/GreenFireBackendApplication.java
@@ -0,0 +1,13 @@
+package sisosolsol.greenfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenFireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenFireBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sisosolsol/greenfire/GreenFireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sisosolsol.greenfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenFireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
