343569ee70e80e96a37814fa3f7a719415ea3ac8
ddalla0425 <hiwgod92@gmail.com>
2024-11-08 18:57:54 +0900

    feat : 초록불 장소 신청 목록 조회 기능에 페이징 추가 구현

--- src/main/java/sisosolsol/greenfire/store/service/StoreService.java
+++ src/main/java/sisosolsol/greenfire/store/service/StoreService.java
@@ -2,11 +2,15 @@ package sisosolsol.greenfire.store.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sisosolsol.greenfire.common.page.Pagination;
+import sisosolsol.greenfire.common.page.SelectCriteria;
 import sisosolsol.greenfire.store.model.dao.StoreMapper;
 import sisosolsol.greenfire.store.model.dto.ApplyStoreListDTO;
 import sisosolsol.greenfire.store.model.dto.StoreListDTO;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class StoreService {
@@ -20,7 +24,19 @@ public class StoreService {
     }
 
     // 초록불 장소 신청 목록 조회
-    public List<ApplyStoreListDTO> getApplyStoreList() {
-        return  storeMapper.findApplyStoreList();
+    public Map<String, Object> getApplyStoreList(int page, int limit) {
+        int totalCount = storeMapper.countWaitingStores(); // `WAITING` 상태 총 개수를 조회하는 메서드
+
+        int buttonAmount = 5; // 페이지 하단에 보일 버튼 수
+        SelectCriteria selectCriteria = Pagination.getSelectCriteria(page, totalCount, limit, buttonAmount);
+
+        List<ApplyStoreListDTO> applyStoreList = storeMapper.findApplyStoreList(selectCriteria);
+
+        Map<String, Object> applyStoreListResponse = new HashMap<>();
+        applyStoreListResponse.put("paging", selectCriteria);
+        applyStoreListResponse.put("applyStoreList", applyStoreList);
+
+        return applyStoreListResponse;
     }
+
 }
