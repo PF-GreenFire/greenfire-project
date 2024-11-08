eda5bbdcce522cbc8cd91fa3b90a7bfaf76f9800
ddalla0425 <hiwgod92@gmail.com>
2024-11-08 18:57:54 +0900

    feat : 초록불 장소 신청 목록 조회 기능에 페이징 추가 구현

--- src/main/java/sisosolsol/greenfire/store/controller/StoreController.java
+++ src/main/java/sisosolsol/greenfire/store/controller/StoreController.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sisosolsol.greenfire.store.model.dto.ApplyStoreListDTO;
 import sisosolsol.greenfire.store.model.dto.StoreListDTO;
 import sisosolsol.greenfire.store.service.StoreService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,10 +27,13 @@ public class StoreController {
         return ResponseEntity.ok(storeList);
     }
 
-    // 초록불 장소 신청 목록 조회 TODO: 추후 관리자만 목록 조회 할수 있겠금 권한 체크 예정
+    // 초록불 장소 신청 목록 조회 TODO: 추후 관리자만 목록 조회 할수 있겠금 권한 체크 예정 / 현재 데이터 3개 조회 되므로 limit 2로 임시 지정 -> 추후 수정 예정
     @GetMapping("/apply/list")
-    public ResponseEntity<List<ApplyStoreListDTO>> getApplyStoreList() {
-        List<ApplyStoreListDTO> applyStoreList = storeService.getApplyStoreList();
+    public ResponseEntity<Map<String, Object>> getApplyStoreList(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "2") int limit
+    ) {
+        Map<String, Object> applyStoreList = storeService.getApplyStoreList(page, limit);
         return ResponseEntity.ok(applyStoreList);
     }
 
--- src/main/java/sisosolsol/greenfire/store/model/dao/StoreMapper.java
+++ src/main/java/sisosolsol/greenfire/store/model/dao/StoreMapper.java
@@ -1,6 +1,7 @@
 package sisosolsol.greenfire.store.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import sisosolsol.greenfire.common.page.SelectCriteria;
 import sisosolsol.greenfire.store.model.dto.ApplyStoreListDTO;
 import sisosolsol.greenfire.store.model.dto.StoreListDTO;
 
@@ -11,6 +12,9 @@ public interface StoreMapper {
     // 초록불 장소 목록 조회 TODO: 현재 위치 정보를 기반으로 반경 지도 목록을 보여주는 것으로 수정 예정
     List<StoreListDTO> findStoreList();
 
-    // 초록불 장소 신청 목록 조회
-    List<ApplyStoreListDTO> findApplyStoreList();
+    // 초록불 장소 신청 목록 토탈 갯수 조회
+    int countWaitingStores();
+
+    // 초록불 장소 신청 목록 페이징 조회
+    List<ApplyStoreListDTO> findApplyStoreList(SelectCriteria selectCriteria);
 }
