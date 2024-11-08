ff6ef300d7ff81b7214d09f9143c64247797a56f
ddalla0425 <hiwgod92@gmail.com>
2024-11-08 17:40:45 +0900

    feat : 초록불 장소 신청 목록 조회 기능 구현

--- src/main/java/sisosolsol/greenfire/store/model/dao/StoreMapper.java
+++ src/main/java/sisosolsol/greenfire/store/model/dao/StoreMapper.java
@@ -1,6 +1,7 @@
 package sisosolsol.greenfire.store.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import sisosolsol.greenfire.store.model.dto.ApplyStoreListDTO;
 import sisosolsol.greenfire.store.model.dto.StoreListDTO;
 
 import java.util.List;
@@ -8,5 +9,8 @@ import java.util.List;
 @Mapper
 public interface StoreMapper {
     // 초록불 장소 목록 조회 TODO: 현재 위치 정보를 기반으로 반경 지도 목록을 보여주는 것으로 수정 예정
-    List<StoreListDTO> getStoreList();
+    List<StoreListDTO> findStoreList();
+
+    // 초록불 장소 신청 목록 조회
+    List<ApplyStoreListDTO> findApplyStoreList();
 }
