d4c86571c5356cfb9ca926d49b318594e6552c23
ddalla0425 <hiwgod92@gmail.com>
2024-11-08 15:30:02 +0900

    refactor : mapper 어노테이션 추가.

--- src/main/java/sisosolsol/greenfire/challenge/model/dao/ChallengeMapper.java
+++ src/main/java/sisosolsol/greenfire/challenge/model/dao/ChallengeMapper.java
@@ -1,9 +1,11 @@
 package sisosolsol.greenfire.challenge.model.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import sisosolsol.greenfire.challenge.model.dto.ChallengeCreateDTO;
 
 import java.util.List;
 
+@Mapper
 public interface ChallengeMapper {
 
     List<ChallengeCreateDTO> getChallenges();
