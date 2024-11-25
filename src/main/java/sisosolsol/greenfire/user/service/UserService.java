b052a917381276070766e1136ab62d7f1d57fcb3
hansol <lhs1119r@gmail.com>
2024-11-25 11:34:37 +0900

    feat: 회원 프로필 정보 수정 기능 구현

--- src/main/java/sisosolsol/greenfire/user/service/UserService.java
+++ src/main/java/sisosolsol/greenfire/user/service/UserService.java
@@ -3,13 +3,14 @@ package sisosolsol.greenfire.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sisosolsol.greenfire.common.exception.BadRequestException;
 import sisosolsol.greenfire.common.exception.type.ExceptionCode;
 import sisosolsol.greenfire.common.security.model.CustomUserDetails;
 import sisosolsol.greenfire.user.model.dao.UserMapper;
 import sisosolsol.greenfire.user.model.dto.UserDTO;
+import sisosolsol.greenfire.user.model.dto.UserUpdateDTO;
 
 import java.util.Optional;
 
@@ -30,4 +31,19 @@ public class UserService {
         }
     }
 
+    @Transactional
+    public UserDTO updateUserProfile(CustomUserDetails loginUser, UserUpdateDTO request) {
+        try {
+            UserDTO user = Optional.ofNullable(userMapper.findByUserCode(loginUser.getId()))
+                    .orElseThrow(() -> new BadRequestException(ExceptionCode.USER_NOT_FOUND));
+
+            userMapper.updateUserProfile(loginUser.getId(), request);
+
+            return userMapper.findByUserCode(loginUser.getId());
+        } catch (DataIntegrityViolationException e) {
+            throw new BadRequestException(ExceptionCode.INVALID_FOREIGN_KEY);
+        } catch (DataAccessException e) {
+            throw new BadRequestException(ExceptionCode.DATABASE_ACCESS_ERROR);
+        }
+    }
 }
