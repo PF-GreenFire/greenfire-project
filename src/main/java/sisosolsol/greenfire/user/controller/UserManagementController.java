9dfe5d13ba2ade6a30857d63bec8d3b6e7b65c35
hansol <lhs1119r@gmail.com>
2024-12-09 21:05:48 +0900

    feat: 관리자 회원 상세 조회 컨트롤러 구현

--- src/main/java/sisosolsol/greenfire/user/controller/UserManagementController.java
+++ src/main/java/sisosolsol/greenfire/user/controller/UserManagementController.java
@@ -2,16 +2,18 @@ package sisosolsol.greenfire.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sisosolsol.greenfire.common.security.model.CustomUserDetails;
+import sisosolsol.greenfire.user.model.dto.UserDetailResponseDTO;
 import sisosolsol.greenfire.user.model.dto.UserListResponseDTO;
 import sisosolsol.greenfire.user.service.UserManagementService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +25,10 @@ public class UserManagementController {
     @GetMapping("/users")
     public ResponseEntity<List<UserListResponseDTO>> getAllUsers(@AuthenticationPrincipal CustomUserDetails loginUser) {
         return ResponseEntity.ok(userManagementService.getAllUsers());
+    }
+
+    @GetMapping("/users/{userCode}")
+    public ResponseEntity<UserDetailResponseDTO> getUserDetail(@PathVariable UUID userCode) {
+        return ResponseEntity.ok(userManagementService.getUserDetail(userCode));
     }
 }
